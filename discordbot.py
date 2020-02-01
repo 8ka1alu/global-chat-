3fb269b999d7a5004a3e550589f5d057bf57a452
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2020-02-01 23:22:41 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,18 +1,17 @@
 import discord 
 import os
-from datetime import datetime
 
-#トークン
+# トークン
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
 
-CHANNEL_ID = 0 #起動メッセージチャンネル
+CHANNEL_ID = 0 # 起動メッセージチャンネル
 ksi_ver = '6.0.1'
 discord_py_ver = '3.7.3'
 
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
 
-#起動メッセージ
+# 起動メッセージ
 @client.event
 async def on_ready():
     print(client.user.name)  # ボットの名前
@@ -27,13 +26,43 @@ async def on_ready():
     await channel.send(f'Discord ver:{discord.__version__}')  # discord.pyのバージョン
     await channel.send('----------------')
     await channel.send('状態：BOT再起動しました。')   
-    await client.change_presence(status=discord.Status.idle,activity=discord.Game(name='創成の女神'))
+    await client.change_presence(status=discord.Status.idle,activity=discord.Game(name='Global Chat'))
     
 
 @client.event
 async def on_message(message):
 
-    if message.author.bot:  # ボットを弾く。
-        return 
+    if message.author.bot:
+        # もし、送信者がbotなら無視する
+        return
+
+# -----以下グローバルチャット------
+
+    GLOBAL_CH_NAME = "hoge-global" # グローバルチャットのチャンネル名
+
+    if message.channel.name == GLOBAL_CH_NAME:
+        # hoge-globalの名前をもつチャンネルに投稿されたので、メッセージを転送する
+
+        await message.delete() # 元のメッセージは削除しておく
+
+        channels = client.get_all_channels()
+        global_channels = [ch for ch in channels if ch.name == GLOBAL_CH_NAME]
+        # channelsはbotの取得できるチャンネルのイテレーター
+        # global_channelsは hoge-global の名前を持つチャンネルのリスト
+
+        embed = discord.Embed(title="hoge-global",
+            description=message.content, color=0x00bfff)
+
+        embed.set_author(name=message.author.display_name, 
+            icon_url=message.author.avatar_url_as(format="png"))
+        embed.set_footer(text=f"{message.guild.name} / {message.channel.name}",
+            icon_url=message.guild.icon_url_as(format="png"))
+        # Embedインスタンスを生成、投稿者、投稿場所などの設定
+
+        for channel in global_channels:
+            # メッセージを埋め込み形式で転送
+            await channel.send(embed=embed)
+
+# -----ここまでグローバルチャット------
 
 client.run(TOKEN)
